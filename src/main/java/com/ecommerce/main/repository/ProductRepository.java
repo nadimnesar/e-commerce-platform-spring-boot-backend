17427f3b51d7868ba2f965d1530113ef5d3bd184
nadimnesar <nadimnesar.cse@gmail.com>
2024-06-16 00:47:50 +0600

    add repository annotation

--- src/main/java/com/ecommerce/main/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/main/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.ecommerce.main.repository;
 
 import com.ecommerce.main.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
