a69aa332351221cd230c77cabf961dfd9037ea0f
nadimnesar <nadimnesar.cse@gmail.com>
2024-06-16 00:47:28 +0600

    getter setter by lombok

--- src/main/java/com/ecommerce/main/model/Product.java
+++ src/main/java/com/ecommerce/main/model/Product.java
@@ -1,10 +1,15 @@
 package com.ecommerce.main.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer productId;
@@ -19,82 +24,4 @@ public class Product {
     private String imageUrl;
     private Double price;
     private Integer stock;
-
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public String getProductTitle() {
-        return productTitle;
-    }
-
-    public void setProductTitle(String productTitle) {
-        this.productTitle = productTitle;
-    }
-
-    public String getProductBrand() {
-        return productBrand;
-    }
-
-    public void setProductBrand(String productBrand) {
-        this.productBrand = productBrand;
-    }
-
-    public ProductCategoryTypes getProductCategory() {
-        return productCategory;
-    }
-
-    public void setProductCategory(ProductCategoryTypes productCategory) {
-        this.productCategory = productCategory;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "productId=" + productId +
-                ", productTitle='" + productTitle + '\'' +
-                ", productBrand='" + productBrand + '\'' +
-                ", productCategory=" + productCategory +
-                ", productDescription='" + productDescription + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", price=" + price +
-                ", stock=" + stock +
-                '}';
-    }
 }
