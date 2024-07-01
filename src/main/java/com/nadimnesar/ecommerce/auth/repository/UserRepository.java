c893b621a4221e1d097e3742c1751d00d125a983
nadimnesar <nadimnesar.cse@gmail.com>
2024-07-01 13:22:53 +0600

    annotation added

--- src/main/java/com/nadimnesar/ecommerce/auth/repository/UserRepository.java
+++ src/main/java/com/nadimnesar/ecommerce/auth/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.nadimnesar.ecommerce.auth.repository;
 
 import com.nadimnesar.ecommerce.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String username);
 }
